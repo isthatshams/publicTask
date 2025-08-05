6559a8d1f94f031bef270007c75ce4fb1ef1fdc1
isthatshams <mohammedmahdishams@gmail.com>
2025-08-05 16:10:12 +0300

    removed console.log, and automated the radio&checkboxes

--- SecondTask/JS/codeUltimate.js
+++ SecondTask/JS/codeUltimate.js
@@ -1,8 +1,6 @@
 document.getElementById('myform').addEventListener('submit', checkAll);
 function checkExist(id) {
     let wordLength = document.getElementById(id).value.length;
-    console.log(wordLength);
-    //use === to make sure it is in the type of 
     if (wordLength === 0) {
         return false;
     }
@@ -11,16 +9,17 @@ function checkExist(id) {
 
 function regExpMatch(regExp, word) {
     if (!word.match(regExp)) {
-        console.log(word + 'did not pass');
         return false;
     }
     return true;
 }
 
-function returnDefault(hint, x) {
+function returnDefault(hint, Element) {
     document.getElementById(hint).innerHTML
         = "";
-    x.style.borderColor = "";
+    if (Element != null) {
+        Element.style.borderColor = "";
+    }
 }
 
 function checkLength(id) {
@@ -31,134 +30,140 @@ function checkLength(id) {
     return true;
 }
 function editStyle(obj, hint, error) {
-    obj.style.borderColor = 'red';
+    if (obj != null) {
+        obj.style.borderColor = 'red';
+    }
     document.getElementById(hint).innerHTML = error;
     return;
 }
 function nameCheck() {
-    let x = document.getElementById('fullname');
+    let Element = document.getElementById('fullname');
     if (!checkExist("fullname")) {
-        editStyle(x, "fullnameHint", "Full Name is Required")
+        editStyle(Element, "fullnameHint", "Full Name is Required")
         return false;
     }
 
-    //^[A-Za-z]+$: the first char is start (if it was inside it is NOT),
-    // the [] is the list,+$ is the end of the string
-    if (!regExpMatch("^[A-Za-z ]+$", "fullname", x.value)) {
-        editStyle(x, "fullnameHint", "Only Alphabets are allowed")
+    if (!regExpMatch("^[A-Za-z ]+$", "fullname", Element.value)) {
+        editStyle(Element, "fullnameHint", "Only Alphabets are allowed")
         return false;
     }
-    returnDefault("fullnameHint", x);
+    returnDefault("fullnameHint", Element);
     return true;
 }
 function userIdCheck() {
-    let x = document.getElementById('userid');
+    let Element = document.getElementById('userid');
 
     if (!checkExist('userid')) {
-        editStyle(x, "useridHint", "User Id is Required")
+        editStyle(Element, "useridHint", "User Id is Required")
         return false;
     }
 
     let wordLength = document.getElementById('userid').value.length;
     if (!(wordLength >= 5 && wordLength <= 12)) {
-        editStyle(x, "useridHint", "User Id length must be of length 5 to 12")
+        editStyle(Element, "useridHint", "User Id length must be of length 5 to 12")
         return false;
     }
-    returnDefault('useridHint', x);
+    returnDefault('useridHint', Element);
     return true;
 }
 
 function passwordCheck() {
-    let x = document.getElementById('password');
+    let Element = document.getElementById('password');
 
     if (!checkExist("password")) {
-        editStyle(x, "passwordHint", "Password is Required")
+        editStyle(Element, "passwordHint", "Password is Required")
         return false;
     }
 
     if (!checkLength("password")) {
-        x.style.borderColor = "red";
-        editStyle(x, "passwordHint", "Password length must be between 5 and 12")
+        Element.style.borderColor = "red";
+        editStyle(Element, "passwordHint", "Password length must be between 5 and 12")
         return false;
 
     }
-    returnDefault('passwordHint', x);
+    returnDefault('passwordHint', Element);
     return true;
 }
 
 function countryCheck() {
-    let x = document.getElementById('country');
+    let Element = document.getElementById('country');
 
     if (!checkExist('country')) {
-        editStyle(x, "countryHint", "Coutry must be Selected")
+        editStyle(Element, "countryHint", "Coutry must be Selected")
         return false;
     }
 
-    returnDefault('countryHint', x);
+    returnDefault('countryHint', Element);
     return true;
 }
 
 function zipcodeCheck() {
-    let x = document.getElementById('zipcode');
+    let Element = document.getElementById('zipcode');
 
     if (!checkExist('zipcode')) {
-        editStyle(x, "zipcodeHint", "ZIP Code is Required")
+        editStyle(Element, "zipcodeHint", "ZIP Code is Required")
         return false;
     }
 
-    // use (/ at the start and end of regExp,
-    // it won't conflict the match Function)
-    if (!regExpMatch(/^\d+$/, x.value)) {
-        editStyle(x, "zipcodeHint", "Only Numbers are allowed")
+    if (!regExpMatch(/^\d+$/, Element.value)) {
+        editStyle(Element, "zipcodeHint", "Only Numbers are allowed")
         return false;
     }
-    returnDefault('zipcodeHint', x);
+    returnDefault('zipcodeHint', Element);
     return true;
 }
 
 function emailCheck() {
-    let x = document.getElementById('email');
+    let Element = document.getElementById('email');
 
     if (!checkExist("email")) {
-        editStyle(x, "emailHint", "Email is Required")
+        editStyle(Element, "emailHint", "Email is Required")
         return false;
     }
-    if (!regExpMatch(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, x.value)) {
-        editStyle(x, "emailHint", "Enter a validate email")
+    if (!regExpMatch(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, Element.value)) {
+        editStyle(Element, "emailHint", "Enter a validate email")
         return false;
     }
 
-    returnDefault('emailHint', x);
+    returnDefault('emailHint', Element);
     return true;
 }
 
 function sexCheck() {
-    let male = document.getElementById('male');
-    let female = document.getElementById('female');
+    let founded;
+    const elementWithNameValue = document.querySelectorAll('[name="sex"]');
 
-    if (male.checked) {
-        returnDefault('sexHint', male);
-        return true;
-    } else if (female.checked) {
-        returnDefault('sexHint', female);
+    elementWithNameValue.forEach(element => {
+        if (element.checked) {
+            returnDefault('sexHint');
+            founded = true;
+        }
+    });
+
+    if (founded == true) {
+        returnDefault('sexHint', null);
         return true;
     }
-    editStyle(x, "sexHint", "Choose your Gender")
+    editStyle(null, "sexHint", "Choose your Gender")
     return false;
 }
 
 function languageCheck() {
-    let english = document.getElementById('english');
-    let russian = document.getElementById('russian');
-    let x = document.getElementById('language');
-    if (english.checked) {
-        returnDefault('languageHint', x);
-        return true;
-    } else if (russian.checked) {
-        returnDefault('languageHint', x);
+    let founded;
+    const elementWithNameValue = document.querySelectorAll('[name="language"]');
+
+    elementWithNameValue.forEach(element => {
+        if (element.checked) {
+            returnDefault('languageHint');
+            founded = true;
+        }
+    });
+
+    if (founded == true) {
+        returnDefault('languageHint', null);
         return true;
     }
-    editStyle(x, "languageHint", "Choose your Gender")
+    editStyle(null, "languageHint", "Choose your Language")
     return false;
 }
 function checkAll() {
